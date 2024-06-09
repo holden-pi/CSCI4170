6e1d4d98f789ce7dfa3b77ae138fe7245e2683e6
holden-pi <holdendpierce@gmail.com>
2024-06-09 15:53:52 -0400

    Add files via upload

---
+++ homework-4-holden-pi/src/main/java/edu/ecu/cs/seng6285/restfulbots/datastore/TextbookService.java
@@ -0,0 +1,116 @@
+package edu.ecu.cs.seng6285.restfulbots.datastore;
+
+import com.google.cloud.datastore.*;
+import edu.ecu.cs.seng6285.restfulbots.models.Textbook;
+import edu.ecu.cs.seng6285.restfulbots.pubsub.Publish;
+import edu.ecu.cs.seng6285.restfulbots.pubsub.Topics;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class TextbookService {
+    private Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    private static final String ENTITY_KIND = "Textbook";
+    private final KeyFactory keyFactory = datastore.newKeyFactory().setKind(ENTITY_KIND);
+
+    public Key createTextbook(Textbook textbook) {
+        Key key = datastore.allocateId(keyFactory.newKey());
+        Entity textbookEntity = Entity.newBuilder(key)
+                .set(Textbook.TITLE, textbook.getTitle())
+                .set(Textbook.AUTHORS, textbook.getAuthors())
+                .set(Textbook.SUBJECT, textbook.getSubject())
+                .set(Textbook.PUBLISHER, textbook.getPublisher())
+                .set(Textbook.YEAR, textbook.getYear())
+                .build();
+        datastore.put(textbookEntity);
+
+        new Publish
+                .Builder()
+                .forProject(Topics.PROJECT_ID)
+                .toTopic(Topics.TEXTBOOK_CREATED)
+                .sendId(key.getId())
+                .build()
+                .publish();
+
+        return key;
+    }
+
+    public List<Textbook> getAllTextbooks() {
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(ENTITY_KIND)
+                .build();
+        Iterator<Entity> entities = datastore.run(query);
+        return buildTextbooks(entities);
+    }
+
+    public List<Textbook> getAllTextbooksForSubject(String subject) {
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(ENTITY_KIND)
+                .setFilter(StructuredQuery.PropertyFilter.eq(Textbook.SUBJECT, subject))
+                .build();
+        Iterator<Entity> entities = datastore.run(query);
+        return buildTextbooks(entities);
+    }
+
+    public Textbook getTextbook(long textbookId) {
+        Key key = keyFactory.newKey(textbookId);
+        Entity entity = datastore.get(key);
+        if (entity != null) {
+            return entityToTextbook(entity);
+        }
+        return null;
+    }
+    
+    public void deleteTextbook(long textbookId) {
+        Key key = keyFactory.newKey(textbookId);
+        datastore.delete(key);
+    
+        new Publish
+                .Builder()
+                .forProject(Topics.PROJECT_ID)
+                .toTopic(Topics.TEXTBOOK_DELETED)
+                .sendId(textbookId)
+                .build()
+                .publish();
+    }
+    
+    public void updateTextbook(Textbook textbook) {
+        Key key = keyFactory.newKey(textbook.getId());
+        Entity textbookEntity = Entity.newBuilder(key)
+                .set(Textbook.TITLE, textbook.getTitle())
+                .set(Textbook.AUTHORS, textbook.getAuthors())
+                .set(Textbook.SUBJECT, textbook.getSubject())
+                .set(Textbook.PUBLISHER, textbook.getPublisher())
+                .set(Textbook.YEAR, textbook.getYear())
+                .build();
+        datastore.update(textbookEntity);
+    
+        new Publish
+                .Builder()
+                .forProject(Topics.PROJECT_ID)
+                .toTopic(Topics.TEXTBOOK_UPDATED)
+                .sendId(textbook.getId())
+                .build()
+                .publish();
+    }
+
+    private List<Textbook> buildTextbooks(Iterator<Entity> entities) {
+        List<Textbook> textbooks = new ArrayList<>();
+        entities.forEachRemaining(entity -> textbooks.add(entityToTextbook(entity)));
+        return textbooks;
+    }
+
+    private Textbook entityToTextbook(Entity entity) {
+        return new Textbook.Builder()
+                .withId(entity.getKey().getId())
+                .withTitle(entity.getString(Textbook.TITLE))
+                .withAuthors(entity.getString(Textbook.AUTHORS))
+                .withSubject(entity.getString(Textbook.SUBJECT))
+                .withPublisher(entity.getString(Textbook.PUBLISHER))
+                .withYear(entity.getLong(Textbook.YEAR))
+                .build();
+    }
+}
